75b82c5c14ce046ca2e5942489851499d4c6164d
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-14 18:48:02 +0100

    add tests

---
+++ tests/test_basic.py
@@ -0,0 +1,13 @@
+from .context import steely
+import unittest
+
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_test(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
